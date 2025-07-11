9694935cdceb5f9d8e8a84917e60ca1f1af6d96f
shahriarsworld <113468427+shahriarsworld@users.noreply.github.com>
2025-07-11 22:09:59 +0600

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import NoticeForm from './components/NoticeForm';
+import ReminderBanner from './components/ReminderBanner';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+function App() {
+  return (
+    <div className="app-container">
+      <h1>📋 BatchBoardV1</h1>
+      <ReminderBanner />
+      <NoticeForm />
+    </div>
+  );
+}
+
+export default App;
